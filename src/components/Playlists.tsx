fb413583fb057d2cf50efc6d6ac9467e73660f69
Ted Shaffer <ted@brightsign.biz>
2021-06-19 13:20:43 -0700

    fixed use of nav bar in Playlists - not sure exactly what was wrong.

--- src/components/Playlists.tsx
+++ src/components/Playlists.tsx
@@ -5,6 +5,11 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { createHashHistory } from 'history';
 
+import { HashRouter } from 'react-router-dom';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
@@ -16,9 +21,16 @@ import { addPlaylistTracksToQueue, getMyPlaylists, openPlaylist, playPlaylist, p
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import NavBar from './NavBar';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
     navBarDiv: {
       verticalAlign: 'top',
       height: '32px',
@@ -118,37 +130,58 @@ const Playlists = (props: PlaylistsProps) => {
     return playlistRows;
   };
 
+  const renderNavbar = () => {
+    return (
+      <NavBar />
+    );
+  };
+
+
   console.log('spotifyPlaylists');
   console.log(props.spotifyPlaylists);
 
-  // <div className={classes.navBarDiv}>
-  // {NavBar}
-  // </div>
+  const navBar = renderNavbar();
 
   if (isArray(props.spotifyPlaylists.items) && props.spotifyPlaylists.items.length > 0) {
+
 
     // table of Playlists
     //    name
     //    tracks.total
     const playlistRows = buildPlaylistRows();
+
     return (
-      <div>
-        <br />
-        <table id='activitiesTable'>
-          <thead>
-            <tr>
-              <th />
-              <th>Name</th>
-              <th>Track Count</th>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {playlistRows}
-          </tbody>
-        </table>
-      </div>
+
+      <HashRouter>
+        <Container maxWidth='xs'>
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Typography component='h1' variant='h5'>
+              Ted Spotify
+            </Typography>
+            <div className={classes.navBarDiv}>
+              {navBar}
+            </div>
+            <div>
+              <br />
+              <table>
+                <thead>
+                  <tr>
+                    <th />
+                    <th>Name</th>
+                    <th>Track Count</th>
+                    <th />
+                    <th />
+                  </tr>
+                </thead>
+                <tbody>
+                  {playlistRows}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </Container>
+      </HashRouter>
     );
   }
 
